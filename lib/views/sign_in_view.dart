4688708562fe4a9330b00dd1985ab01993081c15
Fozan Alkhalawi <fozanalkhalawi@gmail.com>
2021-11-21 20:19:35 +0300

    implement sign up view navigator

--- lib/views/sign_in_view.dart
+++ lib/views/sign_in_view.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:dev_gram/constants.dart';
 import 'package:dev_gram/views/main_view.dart';
+import 'package:dev_gram/views/sign_up_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -17,79 +20,105 @@ class _SignInViewState extends State<SignInView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              kLogoSvgAsset,
-              width: 209,
-              height: 60,
-            ),
-            SizedBox(height: 40),
-            TextField(
-              onChanged: (value) {
-                username = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'phone',
-                fillColor: Color(0xffFAFAFA),
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    kLogoSvgAsset,
+                    width: 209,
+                    height: 60,
+                  ),
+                  SizedBox(height: 40),
+                  TextField(
+                    onChanged: (value) {
+                      username = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      fillColor: Color(0xFFFAFAFA),
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      fillColor: Color(0xffFAFAFA),
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Color(0xff5ACBFE), fontWeight: FontWeight.w700, fontSize: 11),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  MaterialButton(
+                    height: 40,
+                    onPressed: () {
+                      if (username == '7b7b' && password == '123') {
+                        print('Login Successes');
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => MainView()));
+                      } else {
+                        print('Login failed');
+                      }
+                    },
+                    color: Color(0xff5ACBFE),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              GestureDetector(
+                onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignUpView())),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("Don't have an account?",
+                        style: TextStyle(decoration: TextDecoration.none), textAlign: TextAlign.center),
+                    SizedBox(width: 5),
+                    Text('Sign Up!', style: TextStyle(color: Color(0xff5ACBFE)), textAlign: TextAlign.center),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              onChanged: (value) {
-                password = value;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'password',
-                fillColor: Color(0xffFAFAFA),
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Color(0xffC4C4C4), width: 1),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(
-                  color: Color(0xff5ACBFE),
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            MaterialButton(
-              onPressed: () {
-                if (username == '7b7b' && password == '123') {
-                  print('Login Successes');
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MainView()));
-                } else {
-                  print('Login failed');
-                }
-              },
-              color: Color(0xff5ACBFE),
-              child: Text('Login'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
