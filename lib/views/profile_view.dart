ce0da8a358ba709f4cf2d2b8674d55c6beb2985d
Hady <hadysata@gmail.com>
2021-11-22 21:41:35 +0300

    chore: add SignOut button

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,3 +1,5 @@
+import 'package:dev_gram/services/auth.dart';
+import 'package:dev_gram/wrapper.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -5,6 +7,30 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            height: 40,
+            onPressed: () async {
+              await Auth.signOut();
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Wrapper(),
+                  ));
+            },
+            color: Color(0xFFCB280F),
+            child: Text(
+              'SignOut',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
