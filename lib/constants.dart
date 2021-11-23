43af564c048b9d8168e4ed19cd1013c1e71fe22f
Fozan Alkhalawi <fozanalkhalawi@gmail.com>
2021-11-23 15:54:43 +0300

    Implement profile view

--- lib/constants.dart
+++ lib/constants.dart
@@ -15,4 +15,20 @@ const String kSearchSvgAsset = 'assets/svgs/search.svg';
 const String kSeriesSvgAsset = 'assets/svgs/series.svg';
 const String kShareSvgAsset = 'assets/svgs/share.svg';
 const String kShopSvgAsset = 'assets/svgs/shop.svg';
-
+String kWatermelonLink =
+    'https://firebasestorage.googleapis.com/v0/b/fozan-kh.appspot.com/o/watermelon.png?alt=media&token=0c4c219d-4376-432b-a3b7-598e42803bb9';
+String kWatermelonBGLink =
+    'https://firebasestorage.googleapis.com/v0/b/fozan-kh.appspot.com/o/watermelon-background.png?alt=media&token=edf2b655-9cbe-42c1-a300-7cfa607e69e0';
+List<String> kImages = [
+  'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+  'https://images.unsplash.com/photo-1561336313-0bd5e0b27ec8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1600716051809-e997e11a5d52?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1700&q=80',
+  'https://images.unsplash.com/photo-1558383331-f520f2888351?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1578496480157-697fc14d2e55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1558389186-438424b00a32?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1579165466741-7f35e4755660?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1558386621-37b91293b289?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1558388341-890073421493?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
+  'https://images.unsplash.com/photo-1584225064785-c62a8b43d148?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80',
+  'https://images.unsplash.com/photo-1558386619-d39547b31c83?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
+];
--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,35 +1,136 @@
-import 'package:dev_gram/services/auth.dart';
-import 'package:dev_gram/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:dev_gram/constants.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MaterialButton(
-            height: 40,
-            onPressed: () async {
-              await Auth.signOut();
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Wrapper(),
-                  ));
-            },
-            color: Color(0xFFCB280F),
-            child: Text(
-              'SignOut',
-              style: TextStyle(
-                color: Colors.white,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Text('FozanKh', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700)),
+          actions: [
+            SvgPicture.asset(kAddSvgAsset),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SvgPicture.asset(kBurgerSvgAsset),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(colors: [Color(0xFFF7A34B), Color(0xFFDE0046)]),
+                        ),
+                        padding: EdgeInsets.all(2.5),
+                        child: Container(
+                          height: 90,
+                          width: 90,
+                          padding: EdgeInsets.all(2.5),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [Color(0xFFF7A34B), Color(0xFFDE0046)]),
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              fit: BoxFit.contain,
+                              image: NetworkImage(
+                                kWatermelonBGLink,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                          Text('Posts', style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                          Text('Followers', style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                          Text('Following', style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text("Fozan Alkhalawi", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 13)),
+                  SizedBox(height: 5),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt #hashtag",
+                    style: TextStyle(fontSize: 13),
+                  ),
+                  SizedBox(height: 10),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Text('Edit Profile', style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black)),
+                  ),
+                ],
               ),
             ),
+            TabBar(
+              indicatorColor: Colors.black,
+              tabs: [
+                Tab(icon: SvgPicture.asset(kGridSvgAsset)),
+                Tab(icon: SvgPicture.asset(kMentionsSvgAsset)),
+              ],
+            ),
+            Expanded(
+              flex: 1,
+              child: TabBarView(
+                children: [
+                  PhotosGrid(),
+                  PhotosGrid(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PhotosGrid extends StatelessWidget {
+  const PhotosGrid({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(
+        kImages.length,
+        (index) => Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 0.5)),
+          child: Image.network(
+            kImages[index],
+            fit: BoxFit.fitHeight,
           ),
-        ],
+        ),
       ),
     );
   }
